133b52738bda63f7f03dbe11f8a134cc9bcea9cd
deepanshu <deepanshusaini2711@gmail.com>
2026-07-13 14:45:29 +0000

    Initial Setup

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.config.database import create_db_and_tables
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome To ScrubCast API"}
